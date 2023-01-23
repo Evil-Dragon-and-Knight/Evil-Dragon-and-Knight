423d6971b33e89c08024ef341845941c2904d19e
LiF <leesj.dev@gmail.com>
2023-01-23 11:22:43 +0900

    feat: :Loading Control

---
+++ Assets/Scripts/LoadingController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingController : MonoBehaviour
+{
+    private AsyncOperation _operation;
+    private static string _nextScene = "TitleScene";
+
+    private void Start()
+    {
+        Time.timeScale = 1f;
+        Invoke(nameof(StartLoading), 0.3f);
+    }
+
+    public static void LoadScene(string sceneName)
+    {
+        _nextScene = sceneName;
+        SceneManager.LoadScene("LoadingScene");
+    }
+
+    private void StartLoading()
+    {
+        StartCoroutine(LoadSceneProcess());
+    }
+
+    private IEnumerator LoadSceneProcess()
+    {
+        yield return null;
+        _operation = SceneManager.LoadSceneAsync(_nextScene);
+        _operation.allowSceneActivation = false;
+
+        while (!_operation.isDone)
+        {
+            yield return null;
+            if (_operation.progress >= 0.9f)
+            {
+                Invoke(nameof(RequestOK), 0.5f);
+            }
+        }
+    }
+
+    private void RequestOK()
+    {
+        _operation.allowSceneActivation = true;
+    }
+}
